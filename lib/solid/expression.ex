d5645d7351700ed1a161f18df90e3f8243412911
Kitisak Fluke <kitisak.spnr@gmail.com>
2023-03-10 13:03:40 +0700

    Fix cases for expressions

--- lib/solid/expression.ex
+++ lib/solid/expression.ex
@@ -71,14 +71,17 @@ defmodule Solid.Expression do
   false
   """
   @spec eval({value, atom, value} | value) :: boolean
-  def eval({nil, :contains, _v2}), do: false
-  def eval({_v1, :contains, nil}), do: false
   def eval({v1, :contains, v2}) when is_list(v1), do: v2 in v1
-  def eval({v1, :contains, v2}), do: String.contains?(to_string(v1), to_string(v2))
-  def eval({v1, :<=, nil}) when is_number(v1), do: false
-  def eval({v1, :<, nil}) when is_number(v1), do: false
-  def eval({nil, :>=, v2}) when is_number(v2), do: false
-  def eval({nil, :>, v2}) when is_number(v2), do: false
+
+  def eval({v1, :contains, v2}) when is_bitstring(v1) and is_bitstring(v2),
+    do: String.contains?(v1, v2)
+
+  def eval({_v1, :contains, _v2}), do: false
+
+  def eval({v1, op, v2})
+      when op in [:<=, :<, :>=, :>] and (not is_number(v1) or not is_number(v2)),
+      do: false
+
   def eval({v1, op, v2}), do: apply(Kernel, op, [v1, v2])
 
   def eval(value) do
