18affed1c834bdc2d65ac446ffa61c8556e9a497
Kitisak Fluke <kitisak.spnr@gmail.com>
2023-08-14 15:25:09 +0700

    fix cache key

--- lib/solid/utils.ex
+++ lib/solid/utils.ex
@@ -3,21 +3,10 @@ defmodule Solid.Utils do
 
   def apply_lazy(fun) when is_function(fun, 0) do
     task = Task.async(fun)
-    timeout = :timer.minutes(3)
 
-    Solid.LazyCache.setup()
-
-    if res = Solid.LazyCache.get(task) do
-      res
-    else
-      case Task.yield(task, timeout) || Task.shutdown(task) do
-        {:ok, result} ->
-          Solid.LazyCache.put(task, result, :timer.minutes(10))
-          result
-
-        nil ->
-          nil
-      end
+    case Task.yield(task, :timer.minutes(3)) || Task.shutdown(task) do
+      {:ok, result} -> result
+      _ -> nil
     end
   end
 
